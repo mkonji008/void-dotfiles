8978024a6b53a6132e0a10fcdd0bdc151f9dd308
mkonji <mkonji@tuta.io>
2023-10-04 18:46:14 -0400

    directory corrections

--- 01-doas.sh
+++ 01-doas.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # prompt for the username
 read -p "Enter your username: " username
 
@@ -12,13 +11,12 @@ confirmation="${confirmation,,}"
 
 # check if the input is empty or "y" (default) or "yes"
 if [ -z "$confirmation" ] || [ "$confirmation" = "y" ] || [ "$confirmation" = "yes" ]; then
-    echo "Using username: $username"
+	echo "Using username: $username"
 else
-    echo "Exiting..."
-    exit 1
+	echo "Exiting..."
+	exit 1
 fi
 sudo xbps-install -Sy opendoas
 sudo touch /etc/doas.conf
-sudo echo "permit persist $username as root" >> /etc/doas.conf
-sudo echo "ignorepkg=sudo" >> /etc/xbps.d/00-repository-main.conf`
-
+sudo echo "permit persist $username as root" >>/etc/doas.conf
+sudo echo "ignorepkg=sudo" >>/etc/xbps.d/00-repository-main.conf
